fcd2150afaec60ed285ab3e996d05045f63854d3
nsakwa <nsakwa@gmail.com>
2025-08-25 23:53:36 +0300

    add negative numbers

--- test_calculator.py
+++ test_calculator.py
@@ -5,6 +5,9 @@ class TestCalculator(unittest.TestCase):
     def test_add_positive_number(self):
         result= add(2,3)
         self.assertEqual(result, 5)
+    def test_add_negative_number(self):
+        result= add(-2,-3)
+        self.assertEqual(result, -5)
 
 if __name__ == '__main__':
     unittest.main()
